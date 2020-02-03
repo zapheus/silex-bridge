fdaff766932c57eb07c44fec83b6b676aeed7403
Rougin Gutib <rougingutib@gmail.com>
2020-02-03 23:27:52 +0800

    Update UserController

--- tests/Fixture/Controllers/UserController.php
+++ tests/Fixture/Controllers/UserController.php
@@ -6,7 +6,7 @@ namespace Zapheus\Bridge\Silex\Fixture\Controllers;
  * User Controller
  *
  * @package Zapheus
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class UserController
 {
